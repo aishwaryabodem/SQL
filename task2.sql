05491e87ef5ba470249bd456fdbec8906772113a
aishwaryabodem <106659972+aishwaryabodem@users.noreply.github.com>
2023-02-26 16:37:26 +0530

    Add files via upload

---
+++ task2.sql
@@ -0,0 +1,23 @@
+-- create database Football
+-- create table FootballVenue(
+-- venue_id int not null primary key,
+-- venue_name varchar(30) not null,
+-- city_id int not null,
+-- capacity int not null
+-- );
+-- insert into FootballVenue values(1,"Italy",101,1000);
+-- insert into FootballVenue values(2,"USA",102,1001);
+-- insert into FootballVenue values(3,"France",103,1002);
+-- insert into FootballVenue values(4,"Switzerland",104,1003);
+-- insert into FootballVenue values(5,"Sweden",105,1004);
+-- insert into FootballVenue values(6,"West Germany",106,1005);
+-- insert into FootballVenue values(7,"Mexico",107,1006);
+-- insert into FootballVenue values(8,"South Africa",108,1007);
+-- insert into FootballVenue values(9,"Russia",109,1008);
+-- insert into FootballVenue values(10,"Canada",120,1009);
+
+-- select count(venue_name)  from footballvenue;
+
+-- select  venue_name as Location , capacity as Volume from footballvenue
+
+-- delete from  footballvenue where venue_name="Russia";
